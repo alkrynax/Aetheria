bedfd3a3a14ea47c07e38c871a8d68f674aae394
alkrynax <alex@neoderme.com>
2024-10-16 14:21:24 +0200

    Implement first functions

--- Aetheria/src/Aetheria/app.py
+++ Aetheria/src/Aetheria/app.py
@@ -9,18 +9,41 @@ from toga.style.pack import COLUMN, ROW
 
 class AetheriaCompanionApp(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        # Building the main display box
+        self.main_box = toga.Box(style=Pack(direction=COLUMN, padding=10))
+        
+        self.char_button = toga.Button('Fiches Persos')
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = self.main_box
         self.main_window.show()
 
 
 def main():
     return AetheriaCompanionApp()
+
+def showChars(self) : 
+    self.content_box.children.clear()
+    self.content_box.add(toga.Label("Gestion des fiches de personnages"))
+    
+def showDice(self):
+    self.content_box.children.clear()
+    self.content_box.add(toga.Label("Lancer un dé selon le besoin"))
+    
+def showLore(self): 
+    self.content_box.children.clear()
+    self.content_box.add(toga.Label("Accéder à l'histoire et au scénario"))
+
+def showStats(self):
+    self.content_box.children.clear()
+    self.content_box.add(toga.Label("Accéder aux stats du joueur"))
+
+# Lancé de dé en prenant en compte deux params : le dé  (d6/d10/d20) etc et modifier (Bonus/Malus) 
+def DiceRoll(dice,modifier) : 
+    import random
+    result = random.randint(1,dice)
+    if (modifier >=0) :
+        result+=modifier
+    else: 
+        result-=modifier
+    return result
